1971e7657ee300d7886c9e09d2afe53d205651dd
MartinCalvete <63985377+MartinCalvete@users.noreply.github.com>
2020-05-03 11:10:11 -0300

    Add files via upload

---
+++ encuesta_series/encuesta_series/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtSFavorita = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnRegistrar = New System.Windows.Forms.Button()
+        Me.btnBorrar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.grdEncuesta = New System.Windows.Forms.DataGridView()
+        CType(Me.grdEncuesta, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(85, 28)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(121, 20)
+        Me.txtNombre.TabIndex = 0
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(85, 69)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(121, 20)
+        Me.txtApellido.TabIndex = 1
+        '
+        'txtSFavorita
+        '
+        Me.txtSFavorita.FormattingEnabled = True
+        Me.txtSFavorita.Items.AddRange(New Object() {"La Casa de Papel", "Poco Ortodoxa", "GOT", "Stranger Things", "Ninguna de las anteriores"})
+        Me.txtSFavorita.Location = New System.Drawing.Point(85, 106)
+        Me.txtSFavorita.Name = "txtSFavorita"
+        Me.txtSFavorita.Size = New System.Drawing.Size(150, 21)
+        Me.txtSFavorita.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(44, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Apellido"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Serie Favorita"
+        '
+        'btnRegistrar
+        '
+        Me.btnRegistrar.Location = New System.Drawing.Point(357, 194)
+        Me.btnRegistrar.Name = "btnRegistrar"
+        Me.btnRegistrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnRegistrar.TabIndex = 6
+        Me.btnRegistrar.Text = "Registrar"
+        Me.btnRegistrar.UseVisualStyleBackColor = True
+        '
+        'btnBorrar
+        '
+        Me.btnBorrar.Location = New System.Drawing.Point(208, 194)
+        Me.btnBorrar.Name = "btnBorrar"
+        Me.btnBorrar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBorrar.TabIndex = 7
+        Me.btnBorrar.Text = "Borrar"
+        Me.btnBorrar.UseVisualStyleBackColor = True
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(57, 194)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(78, 23)
+        Me.btnCancelar.TabIndex = 8
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'grdEncuesta
+        '
+        Me.grdEncuesta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.grdEncuesta.Location = New System.Drawing.Point(256, 12)
+        Me.grdEncuesta.Name = "grdEncuesta"
+        Me.grdEncuesta.Size = New System.Drawing.Size(240, 150)
+        Me.grdEncuesta.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(508, 235)
+        Me.Controls.Add(Me.grdEncuesta)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnBorrar)
+        Me.Controls.Add(Me.btnRegistrar)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSFavorita)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Name = "Form1"
+        Me.Text = "Registro"
+        CType(Me.grdEncuesta, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtSFavorita As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnRegistrar As Button
+    Friend WithEvents btnBorrar As Button
+    Friend WithEvents btnCancelar As Button
+    Friend WithEvents grdEncuesta As DataGridView
+End Class
---
+++ encuesta_series/encuesta_series/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports MySql.Data.MySqlClient
+'Martin Calvete 3BF
+Public Class Form1
+    Private Sub btnRegistrar_Click(sender As Object, e As EventArgs) Handles btnRegistrar.Click
+
+        Dim conexion As MySqlConnection
+        conexion = New MySqlConnection
+
+        Dim cmd As New MySqlCommand
+        Dim adaptador As MySqlDataAdapter = New MySqlDataAdapter
+        Dim ds As New DataSet
+
+        conexion.ConnectionString = "server=localhost; database=encuesta;Uid=root;Pwd=;"
+
+        Try
+            conexion.Open()
+            cmd.Connection = conexion
+
+            cmd.CommandText = "INSERT INTO encuesta_series(nombre, apellido, serie_favorita) VALUES (@nombre, @apellido, @serie_favorita )"
+            cmd.Prepare()
+
+            cmd.Parameters.AddWithValue("@nombre", txtNombre.Text)
+            cmd.Parameters.AddWithValue("@apellido", txtApellido.Text)
+            cmd.Parameters.AddWithValue("@serie_favorita", txtSFavorita.Text)
+            cmd.ExecuteNonQuery()
+
+            conexion.Close()
+
+            MsgBox("Felicitaciones, usted fue registrado con exito ")
+            txtNombre.Clear()
+            txtApellido.Clear()
+
+            cmd.CommandText = "SELECT * FROM encuesta_series ORDER BY nombre ASC"
+            adaptador.SelectCommand = cmd
+            adaptador.Fill(ds, "Tabla")
+            grdEncuesta.DataSource = ds
+            grdEncuesta.DataMember = "Tabla"
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+
+
+    End Sub
+
+    Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
+
+        txtNombre.Clear()
+        txtApellido.Clear()
+
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+
+        Me.Close()
+
+    End Sub
+End Class
